7c6d546d7fc1d52874a6262636701d3e54619a8a
salineselin <salineselin@gmail.com>
2022-06-04 01:32:18 -0600

    added blank identifier to ignore output of bucket creation its not needed since im no longer logging it

--- main.go
+++ main.go
@@ -9,15 +9,13 @@ func main() {
 	pulumi.Run(
 		func(ctx *pulumi.Context) error {
 			// Create a GCP resource (Storage Bucket)
-			bucket, err := storage.NewBucket(ctx, "my-bucket", &storage.BucketArgs{
+			_, err := storage.NewBucket(ctx, "my-bucket", &storage.BucketArgs{
 				Location: pulumi.String("US"),
 			})
 			if err != nil {
 				return err
 			}
 
-			// Export the DNS name of the bucket
-			ctx.Export("bucketName", bucket.Url)
 			return nil
 		})
 }
