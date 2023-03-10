4202fc64a5569a013009a11c9b51921af102fa28
cerise norling <salineselin@gmail.com>
2023-03-10 13:01:33 -0700

    finalized dns zone configuration

--- gcp/index.ts
+++ gcp/index.ts
@@ -8,55 +8,6 @@ interface zone {
 }
 
 // create zones
-// const zones: zone[] = [
-//   {
-//     name: "norling-io",
-//     args: {
-//       description: "DNS Zone for domain: norling.io",
-//       dnsName: "norling.io.",
-//       dnssecConfig: {
-//         state: "on",
-//         nonExistence: "nsec3",
-//         defaultKeySpecs: [
-//           {
-//             algorithm: "rsasha256",
-//             keyLength: 2048,
-//             keyType: "keySigning",
-//           },
-//           {
-//             algorithm: "rsasha256",
-//             keyLength: 1024,
-//             keyType: "zoneSigning",
-//           },
-//         ],
-//       },
-//     },
-//     opts: {
-//       protect: false,
-//     },
-//   },
-//   {
-//     name: "nuggies-life",
-//     args: {
-//       description: "nuggies are life",
-//       dnsName: "nuggies.life.",
-//     },
-//     opts: {
-//       protect: false,
-//     },
-//   },
-//   {
-//     name: "salinesel-in",
-//     args: {
-//       description: "cloud dns zone for salinesel.in domain",
-//       dnsName: "salinesel.in.",
-//     },
-//     opts: {
-//       protect: false,
-//     },
-//   },
-// ];
-
 const zones: zone[] = [
   {
     name: "norling-io",
@@ -65,25 +16,11 @@ const zones: zone[] = [
         enableLogging: false,
       },
       description: "DNS zone for domain: norling.io",
+      name: "norling-io",
       dnsName: "norling.io.",
       dnssecConfig: {
-        defaultKeySpecs: [
-          {
-            algorithm: "rsasha256",
-            keyLength: 2048,
-            keyType: "keySigning",
-          },
-          {
-            algorithm: "rsasha256",
-            keyLength: 1024,
-            keyType: "zoneSigning",
-          },
-        ],
-        nonExistence: "nsec3",
         state: "on",
       },
-      name: "norling-io",
-      project: "elite-protocol-319303",
     },
     opts: {
       protect: true,
@@ -92,13 +29,9 @@ const zones: zone[] = [
   {
     name: "salinesel-in",
     args: {
-      cloudLoggingConfig: {
-        enableLogging: false,
-      },
       description: "cloud dns zone for salinesel.in domain",
       dnsName: "salinesel.in.",
       name: "salinesel-in",
-      project: "elite-protocol-319303",
     },
     opts: {
       protect: true,
@@ -113,7 +46,6 @@ const zones: zone[] = [
       description: "nuggies are life",
       dnsName: "nuggies.life.",
       name: "nuggies-life",
-      project: "elite-protocol-319303",
     },
     opts: {
       protect: true,
@@ -122,74 +54,7 @@ const zones: zone[] = [
 ];
 
 export function CreateGCPInfrastructure() {
-  //   zones.forEach((zone) => {
-  //     new gcp.dns.ManagedZone(zone.name, zone.args, zone.opts);
-  //   });
-
   zones.forEach((zone) => {
     new gcp.dns.ManagedZone(zone.name, zone.args, zone.opts);
   });
-  //   new gcp.dns.ManagedZone(
-  //     "norling-io",
-  //     {
-  //       cloudLoggingConfig: {
-  //         enableLogging: false,
-  //       },
-  //       description: "DNS zone for domain: norling.io",
-  //       dnsName: "norling.io.",
-  //       dnssecConfig: {
-  //         defaultKeySpecs: [
-  //           {
-  //             algorithm: "rsasha256",
-  //             keyLength: 2048,
-  //             keyType: "keySigning",
-  //           },
-  //           {
-  //             algorithm: "rsasha256",
-  //             keyLength: 1024,
-  //             keyType: "zoneSigning",
-  //           },
-  //         ],
-  //         nonExistence: "nsec3",
-  //         state: "on",
-  //       },
-  //       name: "norling-io",
-  //       project: "elite-protocol-319303",
-  //     },
-  //     {
-  //       protect: true,
-  //     }
-  //   );
-
-  //   new gcp.dns.ManagedZone(
-  //     "salinesel-in",
-  //     {
-  //       cloudLoggingConfig: {
-  //         enableLogging: false,
-  //       },
-  //       description: "cloud dns zone for salinesel.in domain",
-  //       dnsName: "salinesel.in.",
-  //       name: "salinesel-in",
-  //       project: "elite-protocol-319303",
-  //     },
-  //     {
-  //       protect: true,
-  //     }
-  //   );
-
-  //   new gcp.dns.ManagedZone(
-  //     "nuggies-life",
-  //     {
-  //       cloudLoggingConfig: {
-  //         enableLogging: false,
-  //       },
-  //       description: "nuggies are life",
-  //       dnsName: "nuggies.life.",
-  //       name: "nuggies-life",
-  //       project: "elite-protocol-319303",
-  //     },
-  //     {
-  //       protect: true,
-  //     }
-  //   );
 }
