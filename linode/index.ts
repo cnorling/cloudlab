b057c7a9869f85ee18f68cc6473fcf09383b7fd3
cerise norling <salineselin@gmail.com>
2023-03-11 15:24:03 -0700

    programmatically defined inputs from configuration file

--- linode/index.ts
+++ linode/index.ts
@@ -1,16 +1,17 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as linode from "@pulumi/linode";
 
-// globally used variables
-const standardName = "cloudlab";
-const linodeRegion = "us-west";
-
 export function CreateLinodeInfrastructure() {
+  // import config settings
+  const config = new pulumi.Config();
+  const region = config.get("linode:region")!;
+  const resname = config.get("linode:resname")!;
+
   // create a kubernetes cluster
-  new linode.LkeCluster(standardName, {
+  new linode.LkeCluster(resname, {
     k8sVersion: "1.25",
-    label: standardName,
-    region: linodeRegion,
+    label: resname,
+    region: region,
     pools: [
       {
         count: 3,
