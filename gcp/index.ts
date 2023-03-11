0bafb838fc2a9a29b8bf97f0942bfa90eec34309
cerise norling <salineselin@gmail.com>
2023-03-10 17:44:17 -0700

    updated dns type and data structuring to make more sense

--- gcp/index.ts
+++ gcp/index.ts
@@ -1,60 +1,97 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as gcp from "@pulumi/gcp";
+import * as random from "@pulumi/random";
 
 interface zone {
   name: string;
   args: gcp.dns.ManagedZoneArgs;
-  opts: pulumi.CustomResourceOptions;
+  opts?: pulumi.CustomResourceOptions;
 }
 
-// create zones
-const zones: zone[] = [
+interface record {
+  dnsname: string;
+  type: string;
+  rrdatas: string[];
+  ttl?: number;
+  opts?: pulumi.CustomResourceOptions;
+}
+
+interface domain {
+  zone: zone;
+  records: record[];
+}
+
+const domains: domain[] = [
   {
-    name: "norling-io",
-    args: {
-      cloudLoggingConfig: {
-        enableLogging: false,
-      },
-      description: "DNS zone for domain: norling.io",
+    zone: {
       name: "norling-io",
-      dnsName: "norling.io.",
-      dnssecConfig: {
-        state: "on",
+      args: {
+        description: "DNS zone for domain: norling.io",
+        name: "norling-io",
+        dnsName: "norling.io.",
+        dnssecConfig: {
+          state: "on",
+        },
+      },
+      opts: {
+        protect: true,
       },
     },
-    opts: {
-      protect: true,
-    },
-  },
-  {
-    name: "salinesel-in",
-    args: {
-      description: "cloud dns zone for salinesel.in domain",
-      dnsName: "salinesel.in.",
-      name: "salinesel-in",
-    },
-    opts: {
-      protect: true,
-    },
-  },
-  {
-    name: "nuggies-life",
-    args: {
-      cloudLoggingConfig: {
-        enableLogging: false,
+    records: [
+      {
+        dnsname: "norling.io.",
+        type: "A",
+        rrdatas: [
+          "185.199.108.153",
+          "185.199.109.153",
+          "185.199.110.153",
+          "185.199.111.153",
+        ],
       },
-      description: "nuggies are life",
-      dnsName: "nuggies.life.",
-      name: "nuggies-life",
-    },
-    opts: {
-      protect: true,
-    },
+      {
+        dnsname: "norling.io.",
+        type: "MX",
+        rrdatas: ["10 mx1.titan.email.", "20 mx2.titan.email."],
+      },
+      {
+        dnsname: "norling.io.",
+        type: "TXT",
+        rrdatas: ['"v=spf1 include:spf.titan.email ~all"'],
+      },
+      {
+        dnsname: "titan1._domainkey.norling.io.",
+        type: "TXT",
+        rrdatas: [
+          '"v=DKIM1; k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDMw2YCVyR9M6MWEynk7qRcGqNLxax9rl36f9VWPziAe7cAPdoq9TjIh5o0grIqnpzfb1lUejgZ+rnP5h06FLPOK39gL2Ld6y92emrgYMDApXhW5VR3moStLg5gI60DWLtk4E3Nkk/XpFaaLsLLN4Y1xlOO2tbwyRQRhwqAm49PkwIDAQAB"',
+        ],
+      },
+    ],
   },
 ];
 
 export function CreateGCPInfrastructure() {
-  zones.forEach((zone) => {
-    new gcp.dns.ManagedZone(zone.name, zone.args, zone.opts);
+  domains.forEach((domain) => {
+    // create the zone
+    const zone = new gcp.dns.ManagedZone(
+      domain.zone.name,
+      domain.zone.args,
+      domain.zone.opts
+    );
+
+    // create the records
+    domain.records.forEach((record) => {
+      const resourceName = `${record.dnsname}${record.type}`;
+      new gcp.dns.RecordSet(
+        resourceName,
+        {
+          managedZone: zone.name,
+          rrdatas: record.rrdatas,
+          type: record.type,
+          name: record.dnsname,
+          ttl: record.ttl,
+        },
+        record.opts
+      );
+    });
   });
 }
