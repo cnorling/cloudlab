6d4bba1fea9e34932ec1ecd0299176c60cba6a20
cerise norling <salineselin@gmail.com>
2023-04-05 12:00:08 -0600

    updated resourcegroup name

--- azure/index.ts
+++ azure/index.ts
@@ -3,5 +3,8 @@ import * as resources from "@pulumi/azure-native/resources";
 
 export function CreateAzureInfrastructure() {
   // Create an Azure Resource Group
-  const rg = new resources.ResourceGroup("cloudlab");
+  const rg = new resources.ResourceGroup("cloudlab", {
+    location: "eastus",
+    resourceGroupName: "cloudlab",
+  });
 }
