aca2ba2f38f08846aa9b008ae0e3a48e64cefe32
cerise norling <salineselin@gmail.com>
2023-04-05 10:21:23 -0600

    added azure infrastructure

---
+++ azure/index.ts
@@ -0,0 +1,7 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as resources from "@pulumi/azure-native/resources";
+
+export function CreateAzureInfrastructure() {
+  // Create an Azure Resource Group
+  const rg = new resources.ResourceGroup("cloudlab");
+}
--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as gcp from "./gcp";
 import * as linode from "./linode";
+import * as azure from "./azure";
 
 linode.CreateLinodeInfrastructure();
 gcp.CreateGCPInfrastructure();
+azure.CreateAzureInfrastructure();
