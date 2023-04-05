75b25016f3022ce4e2801988d7f9864cae64b991
cerise norling <salineselin@gmail.com>
2023-04-05 12:52:08 -0600

    added k8s stuff temporarily

--- azure/index.ts
+++ azure/index.ts
@@ -1,10 +1,17 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as resources from "@pulumi/azure-native/resources";
+import * as containerservice from "@pulumi/azure-native/containerservice";
 
 export function CreateAzureInfrastructure() {
   // Create an Azure Resource Group
   const rg = new resources.ResourceGroup("cloudlab", {
     location: "eastus",
     resourceGroupName: "cloudlab",
+  });
+
+  // create kubernetes cluster
+  const k8s = new containerservice.ManagedCluster("k8s-cluster", {
+    resourceGroupName: rg.name,
+    kubernetesVersion: "1.25.0",
   });
 }
